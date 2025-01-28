1107d1ef99d34acf258604b239aebe5355bb4fdd
NICOLAS SILVA DE ALMEIDA SANTOS <24122316@dsn127m1246729.cfp127aluno.local>
2025-01-28 17:13:39 -0300

    first commit

---
+++ src/App.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var nome = "Nicolas"
+    var idade = 50
+    idade = 19
+
+    val preco = 18.99
+
+    println("Meu nome é $nome, tenho $idade anos.")
+    print("A soma de 8 e 4 é ${8 + 4}!")
+}
